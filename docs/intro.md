23f0567ab7aa605d2e26bc91403f401e7ecfcbd5
tarjei <tarjei@imerso.com>
2022-08-12 17:38:34 +0200

    Initial commit :tada:

---
+++ docs/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stack-engine
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/stack_engine/core_test.cljs
@@ -0,0 +1,7 @@
+(ns stack-engine.core-test
+  (:require [clojure.test :refer [are deftest is testing]]
+            [stack-engine.core :refer [screen]]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
